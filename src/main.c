aad97251d35736b044231630ccaeb529ca217569
Samarth Verma <samarthverma@icloud.com>
2024-01-02 03:50:22 +0530

    Render score at the top

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <assert.h>
+#include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <raylib.h>
-
 void RenderCell(int x, int y, Color color);
 void SpawnTetramino();
+
+const float AUTODROP_DURATION = 0.2;
+uint64_t score = 0;
 
 typedef enum {
   ACTION_NONE,
@@ -34,90 +36,89 @@ typedef struct {
   int y;
 } TetraminoInstance;
 
-/*
- * Constants
- */
 const int CELL_SIZE = 40;
 const int PLAYFIELD_HIDDEN_ROWS = 40;
 const int PLAYFIELD_ROWS = 20;
 const int PLAYFIELD_COLS = 10;
 const int PADDING_X = 12;
 const int PADDING_Y = 22;
+const int HEADER_HEIGHT = 60;
 
 const int SCREEN_WIDTH = CELL_SIZE * PLAYFIELD_COLS + 2 * PADDING_X;
-const int SCREEN_HEIGHT = CELL_SIZE * PLAYFIELD_ROWS + 2 * PADDING_Y;
+const int SCREEN_HEIGHT =
+    CELL_SIZE * PLAYFIELD_ROWS + 2 * PADDING_Y + HEADER_HEIGHT;
 
 const char *WINDOW_TITLE = "Tetris";
 
 const Color BACKGROUND_COLOR = {240, 240, 240, 255};
 const Color LINE_COLOR = LIGHTGRAY;
 // Cyan: #06b6d4
-#define CYAN                                                                   \
+#define CYAN_COLOR                                                             \
   (Color) { 0x06, 0xb6, 0xd4, 0xff }
 
 // Blue: #2563eb
-#define BLUE                                                                   \
+#define BLUE_COLOR                                                             \
   (Color) { 0x25, 0x63, 0xeb, 0xff }
 
 // Orange: #ea580c
-#define ORANGE                                                                 \
+#define ORANGE_COLOR                                                           \
   (Color) { 0xea, 0x58, 0x0c, 0xff }
 
 // Yellow: #facc15
-#define YELLOW                                                                 \
+#define YELLOW_COLOR                                                           \
   (Color) { 0xfa, 0xcc, 0x15, 0xff }
 
 // Green: #22c55e
-#define GREEN                                                                  \
+#define GREEN_COLOR                                                            \
   (Color) { 0x22, 0xc5, 0x5e, 0xff }
 
 // Purple: #9333ea
-#define PURPLE                                                                 \
+#define PURPLE_COLOR                                                           \
   (Color) { 0x93, 0x33, 0xea, 0xff }
 
 // Red: #dc2626
-#define RED                                                                    \
+#define RED_COLOR                                                              \
   (Color) { 0xdc, 0x26, 0x26, 0xff }
 
 Tetramino TETRAMINO_I = {
     .rotations = {0x0F00, 0x2222, 0x00F0, 0x4444},
-    .color = CYAN,
+    .color = CYAN_COLOR,
     .idx = 0,
 };
 
 Tetramino TETRAMINO_O = {
     .rotations = {0x0660, 0x0660, 0x0660, 0x0660},
-    .color = YELLOW,
+    .color = YELLOW_COLOR,
     .idx = 4,
 };
 
 Tetramino TETRAMINO_T = {
     .rotations = {0x0E40, 0x4C40, 0x4E00, 0x4640},
-    .color = PURPLE,
+    .color = PURPLE_COLOR,
     .idx = 6,
 };
 
 Tetramino TETRAMINO_S = {
     .rotations = {0x06C0, 0x8C40, 0x06C0, 0x8C40},
-    .color = GREEN,
+    .color = GREEN_COLOR,
     .idx = 5,
 };
 
 Tetramino TETRAMINO_Z = {
     .rotations = {0x0C60, 0x4C80, 0x0C60, 0x4C80},
-    .color = RED,
+    .color = RED_COLOR,
     .idx = 2,
 };
 
 Tetramino TETRAMINO_J = {
     .rotations = {0x44C0, 0x8E00, 0x6440, 0x0E20},
-    .color = BLUE,
+    .color = BLUE_COLOR,
     .idx = 1,
 };
 
 Tetramino TETRAMINO_L = {
     .rotations = {0x4460, 0x0E80, 0xC440, 0x2E00},
-    .color = ORANGE,
+    .color = ORANGE_COLOR,
     .idx = 3,
 };
 
@@ -132,7 +133,8 @@ typedef enum {
   CELL_PURPLE
 } CellState;
 
-Color tetraminoColors[] = {CYAN, BLUE, RED, ORANGE, YELLOW, GREEN, PURPLE};
+Color tetraminoColors[] = {CYAN_COLOR,   BLUE_COLOR,  RED_COLOR,   ORANGE_COLOR,
+                           YELLOW_COLOR, GREEN_COLOR, PURPLE_COLOR};
 
 CellState playfield[PLAYFIELD_ROWS][PLAYFIELD_COLS];
 
@@ -154,10 +156,6 @@ bool TimerHasElapsed(Timer *timer) {
   return false;
 }
 
-/*
- * User Interface
- */
-
 void RenderGrid() {
   for (int i = 0; i <= PLAYFIELD_ROWS; i++) {
     int y = SCREEN_HEIGHT - PADDING_Y - i * CELL_SIZE;
@@ -166,7 +164,8 @@ void RenderGrid() {
 
   for (int i = 0; i <= PLAYFIELD_COLS; i++) {
     int x = PADDING_X + i * CELL_SIZE;
-    DrawLine(x, PADDING_Y, x, SCREEN_HEIGHT - PADDING_Y, LINE_COLOR);
+    DrawLine(x, PADDING_Y + HEADER_HEIGHT, x, SCREEN_HEIGHT - PADDING_Y,
+             LINE_COLOR);
   }
 
   for (int i = 0; i < PLAYFIELD_ROWS; i++) {
@@ -191,7 +190,7 @@ void RenderCell(int x, int y, Color color) {
 void InitGame() {
   memset(playfield, 0, sizeof(playfield));
 
-  TimerCreate(&autoDropTimer, 0.2);
+  TimerCreate(&autoDropTimer, AUTODROP_DURATION);
   SpawnTetramino();
 }
 
@@ -243,27 +242,58 @@ bool RenderTetrominoInstance(TetraminoInstance *instance) {
   return true;
 }
 
-/*
- * Input
- */
+void RenderScore(uint64_t score) {
+  char scoreText[32];
+  sprintf(scoreText, "Score: %llu", score);
+
+  // center text
+  int textWidth = MeasureText(scoreText, 24);
+  int x = (SCREEN_WIDTH - textWidth) / 2;
+  int y = PADDING_Y;
+  DrawText(scoreText, x, y, 24, GRAY);
+}
+
 Action HandleInput() {
   if (TimerHasElapsed(&autoDropTimer)) {
     return ACTION_AUTODROP;
-  }
-
-  if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) {
+  } else if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) {
     return ACTION_ROTATE;
-  }
-
-  if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) {
+  } else if (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) {
     return ACTION_LEFT;
-  }
-
-  if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) {
+  } else if (IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) {
     return ACTION_RIGHT;
   }
-
   return ACTION_NONE;
+}
+
+void ResolveClears() {
+  int clearCount = 0;
+  for (int i = 0; i < PLAYFIELD_ROWS; i++) {
+    bool rowIsFull = true;
+    for (int j = 0; j < PLAYFIELD_COLS; j++) {
+      if (playfield[i][j] == CELL_EMPTY) {
+        rowIsFull = false;
+        break;
+      }
+    }
+
+    if (rowIsFull) {
+      for (int j = 0; j < PLAYFIELD_COLS; j++) {
+        playfield[i][j] = CELL_EMPTY;
+      }
+
+      for (int k = i + 1; k < PLAYFIELD_ROWS; k++) {
+        for (int j = 0; j < PLAYFIELD_COLS; j++) {
+          playfield[k - 1][j] = playfield[k][j];
+        }
+      }
+      clearCount++;
+    }
+  }
+
+  if (clearCount > 0) {
+    score += 100 * clearCount;
+  }
 }
 
 void LockTetraminoInstance(TetraminoInstance *instance) {
@@ -278,9 +308,20 @@ void LockTetraminoInstance(TetraminoInstance *instance) {
     playfield[y][x] = instance->tetramino.idx + 1;
   }
 
+  ResolveClears();
+
   free(incomingTetramino);
   incomingTetramino = malloc(sizeof(TetraminoInstance));
   SpawnTetramino();
+}
+
+void CheckGameOver() {
+  for (int i = 0; i < PLAYFIELD_COLS; i++) {
+    if (playfield[PLAYFIELD_ROWS - 1][i] != CELL_EMPTY) {
+      printf("Game Over!\n");
+      exit(0);
+    }
+  }
 }
 
 void HandleAction(Action action) {
@@ -322,6 +363,7 @@ void HandleAction(Action action) {
       LockTetraminoInstance(incomingTetramino);
     }
   }
+  CheckGameOver();
 }
 
 /*
@@ -345,7 +387,7 @@ uint8_t shuffleIndex = 0;
 Tetramino *tetraminos[] = {&TETRAMINO_I, &TETRAMINO_O, &TETRAMINO_T,
                            &TETRAMINO_S, &TETRAMINO_Z, &TETRAMINO_J,
                            &TETRAMINO_L};
-                          
+
 void SpawnTetramino() {
   if (bag == NULL || shuffleIndex == 7) {
     if (bag != NULL) {
@@ -357,7 +399,6 @@ void SpawnTetramino() {
     }
     ShuffleArray(bag, 7);
     shuffleIndex = 0;
-  
   }
   int idx = bag[shuffleIndex++];
   incomingTetramino = malloc(sizeof(TetraminoInstance));
@@ -376,11 +417,11 @@ int main(int argc, char *argv[]) {
     BeginDrawing();
     ClearBackground(BACKGROUND_COLOR);
 
+    RenderScore(score);
     RenderGrid();
 
     Action action = HandleInput();
     HandleAction(action);
-
     RenderTetrominoInstance(incomingTetramino);
 
     EndDrawing();
